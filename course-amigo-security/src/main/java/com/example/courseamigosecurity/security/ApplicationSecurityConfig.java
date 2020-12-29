d01c85cc3bfdfba7635bc965815eaf9f333960e7
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2020-12-29 11:14:56 +0300

    Added starter security dependency. Added class ApplicationSecurityConfig. Basic security configure

---
+++ course-amigo-security/src/main/java/com/example/courseamigosecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.courseamigosecurity.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        /*
+         проверку подлинности любого запроса к приложению с помощью входа
+         в систему на основе формы или базовой проверки подлинности HTTP.
+         */
+    }
+}
