96a0ab9c457967d6f90b9bbbe7693dd4600691c6
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2020-12-29 09:14:43 +0300

    Spring initializer made. No additional dependencies

---
+++ course-amigo-security/src/main/java/com/example/courseamigosecurity/CourseAmigoSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseamigosecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAmigoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAmigoSecurityApplication.class, args);
+	}
+
+}
---
+++ course-amigo-security/src/test/java/com/example/courseamigosecurity/CourseAmigoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseamigosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAmigoSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
