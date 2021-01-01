21f3f6a28c259773deec0324ba269b46e882d8bf
Vishnevskiy Vlaimir Home Laptop <vishnevskiy.vladimir.mbox@gmail.com>
2021-01-01 15:56:11 +0300

    replaced antMatchers with preAuthorized annotaion

--- course-amigo-security/src/main/java/com/example/courseamigosecurity/student/StudentManagementController.java
+++ course-amigo-security/src/main/java/com/example/courseamigosecurity/student/StudentManagementController.java
@@ -1,5 +1,6 @@
 package com.example.courseamigosecurity.student;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,22 +25,26 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getSTUDENTS() {
         System.out.println("GetAll method");
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(Student student){
         System.out.println("PostMethod");
     }
 
     @DeleteMapping(path = "/{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId){
         System.out.println("DelMethod " + studentId);
     }
 
     @PutMapping(path = "/{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId,  Student student){
         System.out.println("UpdateMethod " + studentId);
     }
